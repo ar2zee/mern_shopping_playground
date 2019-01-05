11ff21414da3bd468a4ccdd7848f9b91ba605893
Oganesyan Artur <arturris@mail.ru>
2019-01-04 19:06:07 -0600

    Fixes for Production Variables

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const items = require('./routes/api/items');
 const app = express();
 
 app.use(bodyParser.json());
-// Db conggig
-const db = require('./config/keys').mongoURI;
+// Db config
+let db;
 
 mongoose
   .connect(db, {useNewUrlParser: true})
@@ -25,12 +25,15 @@ app.use('/api/items', items);
 // Serve static asset s if in production
 
 if(process.env.NODE_ENV === 'production') {
+  db = process.env.mongoURI;
   //Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
+} else {
+  db = require('./config/keys').mongoURI;
 }
 
 const port = process.env.PORT || 5000;
